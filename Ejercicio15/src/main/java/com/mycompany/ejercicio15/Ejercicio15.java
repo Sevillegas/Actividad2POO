1f92e967400277ec1a304c71ded5e099d1035719
Sevillegas <sevillegas@unal.edu.co>
2023-03-18 00:13:00 -0500

    Ejercicio15

--- Ejercicio15/src/main/java/com/mycompany/ejercicio15/Ejercicio15.java
+++ Ejercicio15/src/main/java/com/mycompany/ejercicio15/Ejercicio15.java
@@ -9,15 +9,49 @@ public class Ejercicio15 {
 
     public static void main(String[] args) {
         int pesoa, pesob, pesoc, pesod;
-       double porventas, salario1, salario2, salario3;
+       
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Ingrese el valor del peso A 1: ");
+        System.out.println("Ingrese el valor del peso A : ");
         pesoa = entrada.nextInt();
-        System.out.println("Ingrese el valor del peso B 1: ");
+        System.out.println("Ingrese el valor del peso B : ");
         pesob = entrada.nextInt();
-        System.out.println("Ingrese el valor del peso C 1: ");
+        System.out.println("Ingrese el valor del peso C : ");
         pesoc = entrada.nextInt();
-        System.out.println("Ingrese el valor del peso D 1: ");
+        System.out.println("Ingrese el valor del peso D : ");
         pesod = entrada.nextInt();
+        
+        if (pesoa == pesob & pesoa == pesoc){
+            System.out.println("Entonces la esfera D es DIFERENTE ");
+            if(pesod>pesoa){
+                System.out.println("Entonces la esfera D es MAYOR ");
+            }else{
+                System.out.println("Entonces la esfera D es MENOR ");
+            }
+        }else if(pesoa == pesob & pesoa == pesod) {
+            System.out.println("Entonces la esfera C es DIFERENTE ");
+            if(pesoc>pesoa){
+                System.out.println("Entonces la esfera C es MAYOR ");
+            }else{
+                System.out.println("Entonces la esfera C es MENOR ");
+            }
+        }else if(pesoa == pesoc & pesoa == pesod) {
+            System.out.println("Entonces la esfera B es DIFERENTE ");
+            if(pesob>pesod){
+                System.out.println("Entonces la esfera B es MAYOR ");
+            }else{
+                System.out.println("Entonces la esfera B es MENOR ");
+            }
+        }else{
+            System.out.println("LA ESFERA A ES LA DIFERENTE");
+            if(pesoa>pesob){
+                System.out.println("Entonces la esfera A es MAYOR");
+            }else{
+                System.out.println("Entonces la esfera A es MENOR");
+            }
+        }
+        
+          
+    
     }
+    
 }
